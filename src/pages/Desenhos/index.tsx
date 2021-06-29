7a7a9fd6795c9df3e30c6c3ca11767d422ddad08
JoaoGuiBC <joaoguibc@gmail.com>
2021-06-28 23:20:37 -0300

    Drawings page structure (HTML + CSS)

--- src/pages/Desenhos/index.tsx
+++ src/pages/Desenhos/index.tsx
@@ -1,10 +1,59 @@
-// import { Container } from './styles';
-
+import Image from "next/image";
 import { Header } from "../../components/Header";
+
+import {
+  Container,
+  Text,
+  Images,
+  ArtistName,
+  Background,
+  Footer,
+} from './styles';
 
 const Desenhos: React.FC = () => {
   return (
-    <Header title="Desenhos" />
+    <>
+      <Header title="Desenhos" />
+      <Container>
+        <Text>
+          Este espaço foi criado para demonstrar o quanto você é querida e
+          especial para todos nós :&#41;
+        </Text>
+
+        <Images>
+          <ArtistName>
+            <strong>Fulano</strong>
+          </ArtistName>
+          <Background Image="/placeholderImage.png" />
+        </Images>
+
+        <Images>
+          <ArtistName>
+            <strong>Fulano</strong>
+          </ArtistName>
+          <Background Image="/placeholderImage2.png" />
+        </Images>
+
+        <Images>
+          <ArtistName className="artistName">
+            <strong>Fulano</strong>
+          </ArtistName>
+          <Background Image="/placeholderImage.png" />
+        </Images>
+
+        <Images>
+          <ArtistName>
+            <strong>Fulano</strong>
+          </ArtistName>
+          <Background Image="/placeholderImage2.png" />
+        </Images>
+
+        <Footer>
+          <span>Voltar ao início</span>
+          <Image src="/svg/chevronUp.svg" height={16} width={16} />
+        </Footer>
+      </Container>
+    </>
   );
 }
 
---
+++ src/pages/Desenhos/styles.ts
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+interface BackgroundProps {
+  Image: string;
+}
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+
+  max-width: 100vw;
+`;
+
+export const Text = styled.p`
+  letter-spacing: 0.1em;
+  text-align: center;
+  padding: 1.5rem 3.5rem;
+
+  margin: 1rem 0 2.5rem 0;
+`;
+
+export const Images = styled.div`
+  width: 100%;
+
+  & + & > div:first-child {
+    border-radius: 10px;
+    filter: drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.7));
+  }
+`;
+
+export const ArtistName = styled.div`
+  display: flex;
+
+  width: 100%;
+  height: 7rem;
+
+  align-items: center;
+  justify-content: center;
+
+  color: ${({ theme }) => theme.colors.lightText};
+  font-size: 4.5rem;
+  letter-spacing: 0.1em;
+
+  background: ${({ theme }) => theme.colors.secondary};
+  filter: drop-shadow(0px 8px 4px rgba(0, 0, 0, 0.4));
+
+  border-radius: 0px 0px 10px 10px;
+
+  z-index: 3;
+`;
+
+export const Background = styled.div<BackgroundProps>`
+  background-image: url(${({ Image }) => Image});
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+
+  border-radius: 10px;
+
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+
+  width: 100%;
+  height: 4rem;
+
+  align-items: center;
+  justify-content: center;
+
+  color: ${({ theme }) => theme.colors.lightText};
+  font-size: 1rem;
+  letter-spacing: 0.1em;
+
+  background: ${({ theme }) => theme.colors.secondary};
+  filter: drop-shadow(0px -8px 4px rgba(0, 0, 0, 0.4));
+
+  border-radius: 10px 10px 0 0;
+
+  z-index: 3;
+
+  span {
+    display: flex;
+    align-items: center;
+
+    height: 100%;
+    margin-right: 8px;
+    cursor: pointer;
+    user-select: none;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(2);
+    }
+  }
+`;
