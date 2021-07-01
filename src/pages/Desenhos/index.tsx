f8e2b34fe8723b8044debc8a8ce1fa73c707705f
JoaoGuiBC <joaoguibc@gmail.com>
2021-06-30 22:19:35 -0300

    Animate ArtistInfo

--- src/pages/Desenhos/index.tsx
+++ src/pages/Desenhos/index.tsx
@@ -1,4 +1,7 @@
+import { useViewportScroll } from "framer-motion";
 import Image from "next/image";
+import { useState } from "react";
+
 import { Header } from "../../components/Header";
 
 import {
@@ -11,11 +14,27 @@ import {
   Footer,
 } from './styles';
 
-const drawings = [{ id: 1, author: 'Pessoa 1', src: '/placeholderImage.png' }, { id: 2, author: 'Pessoa 2', src: '/placeholderImage2.png' }, { id: 3, author: 'Pessoa 3', src: '/placeholderImage.png' }, { id: 4, author: 'Pessoa 4', src: '/placeholderImage2.png' }];
+const drawings = [{ id: 1, author: 'Pessoa 1', src: '/placeholderImage.png' }, { id: 2, author: 'Pessoa 2', src: '/placeholderImage2.png' }, { id: 3, author: 'Pessoa 3', src: '/placeholderImage.png' }, { id: 4, author: 'Pessoa 4', src: '/placeholderImage2.png' }, { id: 5, author: 'Pessoa 5', src: '/placeholderImage.png' }, { id: 6, author: 'Pessoa 6', src: '/placeholderImage2.png' }, { id: 7, author: 'Pessoa 7', src: '/placeholderImage.png' }, { id: 8, author: 'Pessoa 8', src: '/placeholderImage2.png' }];
 
 const Desenhos: React.FC = () => {
+  const [scrollHeight, setScrollHeight] = useState(0);
+  const { scrollYProgress } = useViewportScroll();
+
+  scrollYProgress.onChange(() => setScrollHeight(scrollYProgress.get() * 100));
+
   function handleScrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  function animateArtistInfo(artistInfoId: number) {
+    const screenPercentage = 100 / drawings.length;
+
+    if (scrollHeight > (screenPercentage * artistInfoId) - 9
+      && scrollHeight < (screenPercentage * (artistInfoId + 1)) - 9) {
+      return 0
+    }
+
+    return '-100%'
   }
 
   return (
@@ -31,7 +50,11 @@ const Desenhos: React.FC = () => {
           <Images key={draw.id}>
             <Separator />
             <Background Image={draw.src}>
-              <ArtistInfo>
+              <ArtistInfo
+                initial={{ x: '-100%' }}
+                animate={{ x: animateArtistInfo(draw.id) }}
+                transition={{ duration: 0.2 }}
+              >
                 <span>Feito por:</span>
                 <strong>{draw.author}</strong>
                 <a href={draw.src} download={`${draw.author}.png`}>
