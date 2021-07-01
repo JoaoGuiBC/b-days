ac79ba5839f52ac6464a7959acaf46e09809533d
JoaoGuiBC <joaoguibc@gmail.com>
2021-06-30 21:02:11 -0300

    Images are now dynamically added to the drawing's page depending of the number of images

--- src/pages/Desenhos/index.tsx
+++ src/pages/Desenhos/index.tsx
@@ -11,8 +11,9 @@ import {
   Footer,
 } from './styles';
 
-const Desenhos: React.FC = () => {
+const drawings = [{ id: 1, author: 'Pessoa 1', src: '/placeholderImage.png' }, { id: 2, author: 'Pessoa 2', src: '/placeholderImage2.png' }, { id: 3, author: 'Pessoa 3', src: '/placeholderImage.png' }, { id: 4, author: 'Pessoa 4', src: '/placeholderImage2.png' }];
 
+const Desenhos: React.FC = () => {
   function handleScrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
@@ -26,61 +27,21 @@ const Desenhos: React.FC = () => {
           especial para todos nós :&#41;
         </Text>
 
-        <Images>
-          <Separator />
-          <Background Image="/placeholderImage.png">
-            <ArtistInfo>
-              <span>Feito por:</span>
-              <strong>Fulano</strong>
-              <a href="/placeholderImage.png" download="draw.png">
-                Download
-                <img src="/svg/download.svg" alt="Download" />
-              </a>
-            </ArtistInfo>
-          </Background>
-        </Images>
-
-        <Images>
-          <Separator />
-          <Background Image="/placeholderImage2.png">
-            <ArtistInfo>
-              <span>Feito por:</span>
-              <strong>Fulano</strong>
-              <a href="/placeholderImage2.png" download="draw.png">
-                Download
-                <img src="/svg/download.svg" alt="Download" />
-              </a>
-            </ArtistInfo>
-          </Background>
-        </Images>
-
-        <Images>
-          <Separator />
-          <Background Image="/placeholderImage.png">
-            <ArtistInfo>
-              <span>Feito por:</span>
-              <strong>Fulano</strong>
-              <a href="/placeholderImage.png" download="draw.png">
-                Download
-                <img src="/svg/download.svg" alt="Download" />
-              </a>
-            </ArtistInfo>
-          </Background>
-        </Images>
-
-        <Images>
-          <Separator />
-          <Background Image="/placeholderImage2.png">
-            <ArtistInfo>
-              <span>Feito por:</span>
-              <strong>Fulano</strong>
-              <a href="/placeholderImage2.png" download="draw.png">
-                Download
-                <img src="/svg/download.svg" alt="Download" />
-              </a>
-            </ArtistInfo>
-          </Background>
-        </Images>
+        {drawings.map(draw => (
+          <Images key={draw.id}>
+            <Separator />
+            <Background Image={draw.src}>
+              <ArtistInfo>
+                <span>Feito por:</span>
+                <strong>{draw.author}</strong>
+                <a href={draw.src} download={`${draw.author}.png`}>
+                  Download
+                  <img src="/svg/download.svg" alt="Download" />
+                </a>
+              </ArtistInfo>
+            </Background>
+          </Images>
+        ))}
 
         <Footer>
           <span onClick={handleScrollToTop}>Voltar ao início</span>
