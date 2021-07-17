40893a0c53582109b5998d979e7bf1bb32799810
JoaoGuiBC <joaoguibc@gmail.com>
2021-07-17 18:34:57 -0300

    issue

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: "AIzaSyDUIbckicKF_G63EM9wOtS7RdEJ6ZqVhWA",
+  authDomain: "b-days-87319.firebaseapp.com",
+  databaseURL: "https://b-days-87319-default-rtdb.firebaseio.com",
+  projectId: "b-days-87319",
+  storageBucket: "b-days-87319.appspot.com",
+  messagingSenderId: "243249385471",
+  appId: "1:243249385471:web:b51a5e051dbe6106a161ca"
 };
 
 if (!firebase.apps.length) {
